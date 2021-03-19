da5d98fa862a987eb2b70344c50bc7955426bcee
Joseff361 <joseff361@outlook.com>
2021-03-19 03:03:52 -0500

    ready to prod

--- src/app/shared/Config.ts
+++ src/app/shared/Config.ts
@@ -1,1 +1,2 @@
-export const baseURL = "http://localhost:8080/cloudinary"
+//export const baseURL = "http://localhost:8080/cloudinary"
+export const baseURL = "https://dorian-image-galery.herokuapp.com"
